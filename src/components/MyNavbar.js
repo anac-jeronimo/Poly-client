450b0a49e7e8003e10c176c7ca76454d6b22fd49
anarita_cavalheiro <anarita.cavalheiro@hotmail.com>
2020-12-04 15:01:34 +0000

    changed the form for log in and updated navbar

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -1,6 +1,6 @@
-
-import AuthService from '../utils/auth';
-import React, { useState } from 'react';
+import AuthService from "../utils/auth";
+import React, { useState } from "react";
+import { Button } from "reactstrap";
 import {
   Collapse,
   Navbar,
@@ -13,103 +13,83 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  NavbarText
-} from 'reactstrap';
+  NavbarText,
+} from "reactstrap";
 
 const MyNavbar = (props) => {
-    
+  const logoutUser = () => {
+    const authService = new AuthService();
+    authService.logout().then(() => {
+      this.props.setCurrentUser(null);
+      localStorage.removeItem("loggedInUser");
+    });
+  };
 
-    const logoutUser = () => {
-        const authService = new AuthService();
-        authService.logout()
-            .then(() => {
-                this.props.setCurrentUser(null);
-                localStorage.removeItem("loggedInUser");
-            })
-    }
+  const [isOpen, setIsOpen] = useState(false);
 
-        const [isOpen, setIsOpen] = useState(false);
-      
-        const toggle = () => setIsOpen(!isOpen);
-    
+  const toggle = () => setIsOpen(!isOpen);
 
-    
-        if(props.loggedInUser) {
-            return (
-                <div>
-                  <Navbar color="light" light expand="md">
-                    <NavbarBrand href="/">Poly</NavbarBrand>
-                    <NavbarToggler onClick={toggle} />
-                    <Collapse isOpen={isOpen} navbar>
-                      <Nav className="mr-auto" navbar>
-                        <NavItem>
-                          <NavLink href="/profile/">Profile</NavLink>
-                        </NavItem>
-                        <NavItem>
-                          <NavLink href="/logout">Logout</NavLink>
-                        </NavItem>
-                        <UncontrolledDropdown nav inNavbar>
-                          <DropdownToggle nav caret>
-                            Learn More
-                          </DropdownToggle>
-                          <DropdownMenu right>
-                            <DropdownItem>
-                              Color Theory
-                            </DropdownItem>
-                            <DropdownItem>
-                              Color Blind Codes
-                            </DropdownItem>
-                            <DropdownItem>
-                              Facts about color blindness
-                            </DropdownItem>
-                              Credits
-                          </DropdownMenu>
-                        </UncontrolledDropdown>
-                      </Nav>
-                    </Collapse>
-                  </Navbar>
-                </div>
-              );
-        } else {
-          return (
-          <div>
-                  <Navbar color="light" light expand="md">
-                    <NavbarBrand href="/">Poly</NavbarBrand>
-                    <NavbarToggler onClick={toggle} />
-                    <Collapse isOpen={isOpen} navbar>
-                      <Nav className="mr-auto" navbar>
-                        <NavItem>
-                          <NavLink href="/signup">Signup</NavLink>
-                        </NavItem>
-                        <NavItem>
-                          <NavLink href="/login">Login</NavLink>
-                        </NavItem>
-                        <UncontrolledDropdown nav inNavbar>
-                          <DropdownToggle nav caret>
-                            Learn More
-                          </DropdownToggle>
-                          <DropdownMenu right>
-                            <DropdownItem>
-                              Color Theory
-                            </DropdownItem>
-                            <DropdownItem>
-                              Color Blind Codes
-                            </DropdownItem>
-                            <DropdownItem>
-                              Facts about color blindness
-                            </DropdownItem>
-                            <DropdownItem>
-                              Credits
-                              </DropdownItem>
-                          </DropdownMenu>
-                        </UncontrolledDropdown>
-                      </Nav>
-                    </Collapse>
-                  </Navbar>
-                </div>
-          )
-        }    
-    }
+  if (props.loggedInUser) {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Poly</NavbarBrand>
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/profile/">Profile</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/logout">Logout</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Learn More
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Color Theory</DropdownItem>
+                  <DropdownItem>Color Blind Codes</DropdownItem>
+                  <DropdownItem>Facts about color blindness</DropdownItem>
+                  Credits
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Poly</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/signup">Signup</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/login">Login</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Learn More
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Color Theory</DropdownItem>
+                  <DropdownItem>Color Blind Codes</DropdownItem>
+                  <DropdownItem>Facts about color blindness</DropdownItem>
+                  <DropdownItem>Credits</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+};
 
 export default MyNavbar;
 
