4d198c23c0b307a962d3fe287aa7b2b7b039af2d
Ana Jerónmio <anac_jeronimo@hotmail.com>
2020-12-08 14:42:14 +0000

    facts css

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -4,7 +4,7 @@ function Facts() {
   return (
     <div>
     <div className="container-fluid">
-    <div className="row">
+    <div className="row facts-page">
 
     <div className="col-sm text-div ">
 
@@ -12,30 +12,28 @@ function Facts() {
       <h3> What is Color-Blindness </h3>
       
       <p>
-        Color-blindness is the inability to distinguish the differences between
+      *  Color-blindness is the inability to distinguish the differences between
         certain colors.
       </p>
       <p>
-        This condition results from an absence of color-sensitive pigment in the
-        cone cells of the retina,
-      </p>
-      <p> the nerve layer at the back of the eye.</p>
+      *  This condition results from an absence of color-sensitive pigment in the
+        cone cells of the retina, the nerve layer at the back of the eye.</p>
 
-      <p>Most color vision problems are inherited and are present at birth.</p>
+      <p>*  Most color vision problems are inherited and are present at birth.</p>
       <p>
         
-        Approximately 1 out of 12 males and 1 out of 20 women are color blind.
+      *  Approximately 1 out of 12 males and 1 out of 20 women are color blind.
       </p>
        
 
       <h3> What does a color-blind person see? </h3>
       <p>
-        A person with color-blindness has trouble seeing red, green, blue, or
+      *  A person with color-blindness has trouble seeing red, green, blue, or
         mixtures of these colors.
       </p>
       <p>
        
-        The most common type is red-green color-blindness, where red and green
+      * The most common type is red-green color-blindness, where red and green
         are seen as the same color.
       </p>
       </div>
@@ -49,7 +47,8 @@ function Facts() {
       </h3>
 
       <img className="img-stripes"
-        src="images/colorblind.JPG"
+       // src="images/colorblind.JPG"
+       src="images\eyeTest_know_color_blindness.jpg"
         alt="color blindness spectrum comparison"
       />
       </div>
