f9fa2b0086d5127f8db8f6b4af5c657ca32a43a2
anarita_cavalheiro <anarita.cavalheiro@hotmail.com>
2020-12-08 12:45:55 +0000

    new changes to scan css

--- src/components/Scan.js
+++ src/components/Scan.js
@@ -38,7 +38,7 @@ class Scan extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="scan-return-div">
         <form onSubmit={this.handleFormSubmit} encType="multipart/form-data">
           <div className="form-group">
             <div>
@@ -52,26 +52,28 @@ class Scan extends React.Component {
             <button className="button-upload">Upload</button>
           </div>
         </form>
-        {this.state.fileUrlOnCloudinary ? (
-          <div className="uploaded-img">
-            <img src={this.state.fileUrlOnCloudinary} />
-          </div>
-        ) : (
-          <div>Uploading...</div>
-        )}
-        <div>
-          {this.state.colorCode ? (
-            <div className="color-code-result">
-              <div className="color-code-result-img">
-                <img src={this.state.colorCode} />
-              </div>
-              <div className="color-code-result-name">
-                {this.state.colorName}
-              </div>
+        <div className="scan-retun-results-cloudinary">
+          {this.state.fileUrlOnCloudinary ? (
+            <div className="uploaded-img">
+              <img src={this.state.fileUrlOnCloudinary} />
             </div>
           ) : (
-            <div>Loading...</div>
+            <div>Uploading...</div>
           )}
+          <div>
+            {this.state.colorCode ? (
+              <div className="color-code-result">
+                <div className="color-code-result-img">
+                  <img src={this.state.colorCode} />
+                </div>
+                <div className="color-code-result-name">
+                  {this.state.colorName}
+                </div>
+              </div>
+            ) : (
+              <div>Loading...</div>
+            )}
+          </div>
         </div>
       </div>
     );
