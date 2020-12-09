a349d077bd321b24a1dea3d557c77d8dd958f5f4
anarita_cavalheiro <anarita.cavalheiro@hotmail.com>
2020-12-09 15:47:54 +0000

    saved and commited loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,33 +1,29 @@
-/* import React from "react";
+import React from "react";
 import { css } from "@emotion/core";
 import ClipLoader from "react-spinners/ClipLoader";
 
-const override = css`
-  display: block;
-  margin: 0 auto;
-  border-color: red;
-`;
- 
 class Loader extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: true
+      loading: true,
     };
   }
- 
+
   render() {
     return (
       <div className="sweet-loading">
-        <ClipLoader
-          css={override}
-          size={150}
-          color={"#257AEC"}
-          loading={this.state.loading}
-        />
+        <div className="override">
+          <ClipLoader
+            //css={override}
+            size={150}
+            color={"#257AEC"}
+            loading={this.state.loading}
+          />
+        </div>
       </div>
     );
   }
 }
 
-export default Loader; */
+export default Loader;
